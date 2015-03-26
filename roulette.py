62b1764c2e309d0b2608b031f3b345deaaf4047b
Milad <khakpour.m@icloud.com>
2015-03-26 20:39:53 +0100

    Add Roulette

---
+++ roulette.py
@@ -0,0 +1,25 @@
+import random
+from sys import exit
+
+print "** Welcome to Group 7 Roulette **"
+
+print "Enter your name :"
+player = raw_input("> ")
+
+print "How much money do you bring to a Roulette table?"
+money = int(raw_input("> $ "))
+
+red = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+black = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
+green = [0]
+
+print """
+ *** Please choose your bet type :
+(1)Straight
+(2)Manque
+(3)Passe
+(4)Rouge ou Noir
+(5)Pair ou Impair
+(6)Dozen Bets
+(7)Column Bets
+"""
