c9dc5a44dc5ce907972802f5479d4666bc0a14eb
Milad <khakpour.m@icloud.com>
2015-03-26 22:13:06 +0100

    Edit

--- roulette.py
+++ roulette.py
@@ -2,17 +2,19 @@
 import random
 from sys import exit
 
+d = {1 : "Straight", 2 : "Manque", 3 : "Passe", 4 : "Rouge ou Noir", 5 : "Pair ou Impair" , 6 : "Dozen Bets", 7 : "Column Bets"}
+red = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+black = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
+green = 0
+
 print "** Welcome to Group 7 Roulette **"
 
 print "Enter your name :"
 player = raw_input("> ")
 
 print "Hi %s, how much money do you want to bring to a Roulette table?" % player
-money = int(raw_input("> € "))
 
-red = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
-black = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
-green = [0]
+money = float(raw_input("> € "))
 
 print """
  *** Please choose your bet type :
@@ -23,5 +25,13 @@ print """
 (5)Pair ou Impair
 (6)Dozen Bets
 (7)Column Bets
+
+(8)Exit
 """
-bet_type = raw_input("> ")
+bet_type = int(raw_input("> "))
+
+for i in d:
+    if bet_type == i:
+        print "Good ! {} ".format(d[i])
+    else:
+        exit()
