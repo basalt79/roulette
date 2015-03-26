f5d6c81ad0baae11825968281ad6c57c37cb33bc
Milad <khakpour.m@icloud.com>
2015-03-26 20:51:32 +0100

    utf-8

--- roulette.py
+++ roulette.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import random
 from sys import exit
 
@@ -6,8 +7,8 @@ print "** Welcome to Group 7 Roulette **"
 print "Enter your name :"
 player = raw_input("> ")
 
-print "How much money do you bring to a Roulette table?"
-money = int(raw_input("> $ "))
+print "Hi %s, how much money do you want to bring to a Roulette table?" % player
+money = int(raw_input("> € "))
 
 red = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
 black = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
@@ -23,3 +24,4 @@ print """
 (6)Dozen Bets
 (7)Column Bets
 """
+bet_type = raw_input("> ")
