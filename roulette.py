f04b9353764ec8a5f8bb02188df54023c8e33891
Li <lin828454@gmail.com>
2015-04-06 18:03:58 +0200

    added manque_ou_passe and spinRoulette functions

--- roulette.py
+++ roulette.py
@@ -2,50 +2,88 @@
 import random
 from sys import exit
 
-
-
+# function for straight bet type
 def straight(input):
     print "You play straight! Enter your bets: "
 
     while True:
-        amount = raw_input("I play Euro: ")
+        amount = raw_input("I play EUR: ")
         if amount.isdigit():
             amount = float(amount)
             if (input - amount >= 0) & (amount >= 0):
-                bet = int(raw_input("{} Euro on: ".format(amount)))
+                bet = int(raw_input("{} EUR on: ".format(amount)))
                 if 0 <= bet <= 36:
-                    print "{} Euro on {}. No more bets please!".format(amount, bet)
-                    rnd = random.randint(0,36)
-                    if rnd in red:
-                        print "{} Red".format(rnd)
-                    if rnd in black:
-                        print "{} Black".format(rnd)
+                    print "{} EUR on {}. No more bets please!".format(amount, bet)
+                    rnd = spinRoulette()
 
                     if rnd == bet:
-                        print "You Win {} Euro".format(amount*35)
+                        print "You Win {} EUR".format(amount*35)
                         input += amount * 35
-                        print "New bank acount: {} Euro".format(input)
-                        break
+                        print "New bank acount: {} EUR".format(input)
+                        return input
                     else:
-                        print "You Lose {} Euro".format(amount)
+                        print "You Lose {} EUR".format(amount)
                         input -= amount
-                        print "New bank account: {} Euro".format(input)
-                        break
+                        print "New bank account: {} EUR".format(input)
+                        return input
                 else:
                     print "invalid bet... try again!"
                     continue
-
+            else:
+                print "Irregular Bet! Bank Account: {} EUR".format(input)
         else:
             print "invalid bet..."
 
-    return input
-
-def manque(input):
-    print "manque"
     return
 
-def passe(input):
-    print "passe"
+# function for Manque or Passe bet type
+def manque_ou_passe(input):
+    print "You play Manque ou Passe! Enter your bets: "
+    while True:
+        amount = raw_input("I play EUR: ")
+        if amount.isdigit():
+            amount = float(amount)
+            if input - amount >= 0:
+                bet = raw_input("{} EUR on (1)Manque or (2)Passe?: ".format(amount))
+                if bet.isdigit():
+                    bet = int(bet);
+                    if bet == 1:
+                        print "{} EUR on Manque. No more bets please!".format(amount)
+                        rnd = spinRoulette()
+
+                        if 1 <= rnd <= 18:
+                            print "You win {} EUR".format(amount)
+                            input += amount
+                            print "New bank acount: {} EUR".format(input)
+                            return input
+                        else:
+                            print "You Lose {} EUR".format(amount)
+                            input -= amount
+                            print "New bank account: {} EUR".format(input)
+                            return input
+
+                    elif bet == 2:
+                        print "{} EUR on Passe. No more bets please!".format(amount)
+                        rnd = spinRoulette()
+
+                        if 19 <= rnd <= 36:
+                            print "You win {} EUR".format(amount)
+                            input += amount
+                            print "New bank acount: {} EUR".format(input)
+                            return input
+                        else:
+                            print "You Lose {} EUR".format(amount)
+                            input -= amount
+                            print "New bank account: {} EUR".format(input)
+                            return input
+                    else:
+                        print "invalid choice..."
+                else:
+                    print "invalid bet..."
+            else:
+                print "Irregular Bet! Bank Account: {} EUR".format(input)
+        else:
+            print "invalid bet..."
     return
 
 def rouge_ou_noir(input):
@@ -64,26 +102,38 @@ def column_bets(input):
     print "column_bets"
     return
 
+# function for spinning the roulette and return the number with color
+def spinRoulette():
+    rnd = random.randint(0,36)
+    if rnd in red:
+        print "{} Red".format(rnd)
+    if rnd in black:
+        print "{} Black".format(rnd)
+    return rnd
+
+# function for printing rules and information about the different bet types
 def helpme(*args):
     print """
     *** Roulette Guide ***
     (1) Straight (or Single) a bet on a single number [35 to 1]
-    (2) 1 to 18 (Manque) a bet on one of the rst 18 numbers. [1 to 1]
-    (3) 19 to 36 (Passe) a bet on the high 18 numbers. [1 to 1]
-    (4) Red or Black (Rouge ou Noir) a bet on which color the roulette wheel will show. [1 to 1]
-    (5) Even or odd (Pair ou Impair) a bet on even or odd nonzero number. [1 to 1]
-    (6) Dozen Bets a bet on the rst 12 (1-12), second 12 (13-24) or third 12 (25-36) numbers. [2 to 1]
-    (7) Column Bets a bet on one of the three vertical lines e.g.: 1-4-7-10 . . . [2 to 1]
+    (2) Manque (1 to 18) a bet on one of the first 18 numbers. [1 to 1]
+        or Passe (19 to 36) a bet on the high 18 numbers. [1 to 1]
+    (3) Red or Black (Rouge ou Noir) a bet on which color the roulette wheel will show. [1 to 1]
+    (4) Even or odd (Pair ou Impair) a bet on even or odd nonzero number. [1 to 1]
+    (5) Dozen Bets a bet on the rst 12 (1-12), second 12 (13-24) or third 12 (25-36) numbers. [2 to 1]
+    (6) Column Bets a bet on one of the three vertical lines e.g.: 1-4-7-10 . . . [2 to 1]
 
     The numbers in [] represent the payout of each bet e.g. [2 to 1] means that you get 2 times
     the amount you bet from the bank. Good Luck!
     """
     return
 
-d = {1 : straight, 2 : manque, 3 : passe, 4 : rouge_ou_noir, 5 : pair_ou_impair, 6 : dozen_bets, 7 : column_bets, 9 : helpme}
+# use dictionary as data structure
+d = {1 : straight, 2 : manque_ou_passe, 3 : rouge_ou_noir,  4 : pair_ou_impair, 5 : dozen_bets, 6 : column_bets, 8 : helpme}
 red = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
 black = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
-green = 0
+
+### Here the programm starts ###
 
 print "*** Welcome to Group 7 Roulette ***"
 
@@ -101,13 +151,13 @@ print "Hi {}!".format(player)
 money = None
 while True:
     print "How much money do you want to bring to a Roulette table?"
-    money = raw_input("Euro: ")
+    money = raw_input("EUR: ")
 
     if money.isdigit():
         money = float(money)
 
         if money > 0:
-            print "You play with {:.2f} Euro".format(money)
+            print "You play with {:.1f} EUR".format(money)
             break
         else:
             print "{} is not a valid amount...".format(money)
@@ -117,15 +167,14 @@ while True:
 print """
  *** Please choose your bet type :
 (1)Straight
-(2)Manque
-(3)Passe
-(4)Rouge ou Noir
-(5)Pair ou Impair
-(6)Dozen Bets
-(7)Column Bets
+(2)Manque ou Passe
+(3)Rouge ou Noir
+(4)Pair ou Impair
+(5)Dozen Bets
+(6)Column Bets
 
-(8)Exit
-(9)Help
+(7)Exit
+(8)Help
 """
 
 
@@ -138,13 +187,13 @@ while True:
     bet_type = raw_input("Bet type: ")
     if bet_type.isdigit():
         bet_type = int(bet_type)
-        if 1 <= bet_type <= 7:
+        if 1 <= bet_type <= 6:
             func = d[bet_type]
             money = func(money)
-        elif bet_type == 8:
-            print "Byebye!"
+        elif bet_type == 7:
+            print "Byebye! See You again!"
             exit()
-        elif bet_type == 9:
+        elif bet_type == 8:
             func = d[bet_type]
             func()
         else:
